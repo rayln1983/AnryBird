d41b1841b7a2567b1f082402ed78318386070814
rayln1983 <rayln@raylntekiMac-Pro.local>
2013-10-19 07:57:07 +0800

    update

--- AnryBird/SlingshotLeft.m
+++ AnryBird/SlingshotLeft.m
@@ -31,7 +31,7 @@
     
     shape_ = cpBoxShapeNew(staticBody, WIDTH, HEIGHT);
 	cpShapeSetElasticity( shape_, 0.0f );
-	cpShapeSetFriction( shape_, 0.5f );
+	cpShapeSetFriction( shape_, 0.0f );
     cpShapeSetCollisionType(shape_, COLLISION_TYPE);
     cpShapeSetLayers(shape_, 3);
 
